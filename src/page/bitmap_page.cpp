37fbfe10eb2020bdbde15515ae9608cb78cfa057
Weilin Chang <95030066+xishanqiming@users.noreply.github.com>
2023-05-25 20:19:13 +0800

    Update bitmap_page.cpp

--- src/page/bitmap_page.cpp
+++ src/page/bitmap_page.cpp
@@ -1,21 +1,41 @@
 #include "page/bitmap_page.h"
-
+#include <cmath>
 #include "glog/logging.h"
 
 /**
  * TODO: Student Implement
  */
 template <size_t PageSize>
-bool BitmapPage<PageSize>::AllocatePage(uint32_t &page_offset) {
-  return false;
+bool BitmapPage<PageSize>::AllocatePage(uint32_t &page_offset) 
+{
+  if (allocate_pages.test(page_offset) || page_offset >= 8 * MAX_CHARS) 
+  {
+    return false;
+  } 
+  else 
+  {
+    allocate_pages.set(page_offset, 1);
+    return true;
+  }
 }
 
 /**
  * TODO: Student Implement
  */
 template <size_t PageSize>
-bool BitmapPage<PageSize>::DeAllocatePage(uint32_t page_offset) {
-  return false;
+bool BitmapPage<PageSize>::DeAllocatePage(uint32_t page_offset) 
+{
+  if (allocate_pages.test(page_offset)) 
+  {
+    allocate_pages.set(page_offset, 0);
+    next_free_page_ = page_offset;
+    page_allocated_--;
+    return true;
+  } 
+  else 
+  {
+    return false;
+  }
 }
 
 /**
@@ -23,12 +43,18 @@ bool BitmapPage<PageSize>::DeAllocatePage(uint32_t page_offset) {
  */
 template <size_t PageSize>
 bool BitmapPage<PageSize>::IsPageFree(uint32_t page_offset) const {
-  return false;
+  return !allocate_pages.test(page_offset);
 }
 
 template <size_t PageSize>
 bool BitmapPage<PageSize>::IsPageFreeLow(uint32_t byte_index, uint8_t bit_index) const {
   return false;
+}
+
+template <size_t PageSize>
+bool BitmapPage<PageSize>::IsBitmapFull() 
+{
+  return page_allocated_ == 8 * MAX_CHARS;
 }
 
 template class BitmapPage<64>;
