77cb331c0f6628ad4089baae8e29eeaec73419a2
Weilin Chang <95030066+xishanqiming@users.noreply.github.com>
2023-05-28 11:43:53 +0800

    Update field.h

--- src/include/record/field.h
+++ src/include/record/field.h
@@ -2,12 +2,11 @@
 #define MINISQL_FIELD_H
 
 #include <cstring>
-#include <string>
 
 #include "common/config.h"
 #include "common/macros.h"
-#include "record/type_id.h"
 #include "record/types.h"
+#include "record/type_id.h"
 
 class Field {
   friend class Type;
@@ -18,7 +17,7 @@ class Field {
 
   friend class TypeFloat;
 
- public:
+public:
   explicit Field(const TypeId type) : type_id_(type), len_(FIELD_NULL_LEN), is_null_(true) {}
 
   ~Field() {
@@ -62,7 +61,7 @@ class Field {
   }
 
   // copy constructor
-  explicit Field(const Field &other) {
+  Field(const Field &other) {
     type_id_ = other.type_id_;
     len_ = other.len_;
     is_null_ = other.is_null_;
@@ -81,25 +80,58 @@ class Field {
     return *this;
   }
 
-  inline bool IsNull() const { return is_null_; }
-
-  inline uint32_t GetLength() const { return Type::GetInstance(type_id_)->GetLength(*this); }
-
-  inline TypeId GetTypeId() const { return type_id_; }
-
-  inline const char *GetData() const { return Type::GetInstance(type_id_)->GetData(*this); }
-
-  inline uint32_t SerializeTo(char *buf) const { return Type::GetInstance(type_id_)->SerializeTo(*this, buf); }
-
-  inline static uint32_t DeserializeFrom(char *buf, const TypeId type_id, Field **field, bool is_null) {
-    return Type::GetInstance(type_id)->DeserializeFrom(buf, field, is_null);
+  inline bool IsNull() const {
+    return is_null_;
   }
 
-  inline uint32_t GetSerializedSize() const { return Type::GetInstance(type_id_)->GetSerializedSize(*this, is_null_); }
+  inline uint32_t GetLength() const {
+    return Type::GetInstance(type_id_)->GetLength(*this);
+  }
 
-  inline bool CheckComparable(const Field &o) const { return type_id_ == o.type_id_; }
+  inline const char *GetData() const {
+    return Type::GetInstance(type_id_)->GetData(*this);
+  }
 
-  inline CmpBool CompareEquals(const Field &o) const { return Type::GetInstance(type_id_)->CompareEquals(*this, o); }
+  inline int32_t GetInt() const 
+  {
+    return value_.integer_;
+  }
+
+  inline float GetFloat() const 
+  {
+    return value_.float_;
+  }
+
+  inline TypeId GetType() const 
+  {
+    return this->type_id_;
+  }
+
+  inline int GetWidth()
+  {
+    return this->len_;
+  }
+
+  inline uint32_t SerializeTo(char *buf) const {
+    return Type::GetInstance(type_id_)->SerializeTo(*this, buf);
+  }
+
+  inline static uint32_t DeserializeFrom(char *buf, const TypeId type_id, Field **field, bool is_null, MemHeap *heap) {
+    return Type::GetInstance(type_id)->DeserializeFrom(buf, field, is_null, heap);
+  }
+
+  inline uint32_t GetSerializedSize() const {
+    return Type::GetInstance(type_id_)->GetSerializedSize(*this, is_null_);
+  }
+
+  inline bool CheckComparable(const Field &o) const {
+    return type_id_ == o.type_id_;
+  }
+
+  inline CmpBool CompareEquals(const Field &o) const 
+  {
+    return Type::GetInstance(type_id_)->CompareEquals(*this, o); 
+  }
 
   inline CmpBool CompareNotEquals(const Field &o) const {
     return Type::GetInstance(type_id_)->CompareNotEquals(*this, o);
@@ -129,22 +161,7 @@ class Field {
     std::swap(first.manage_data_, second.manage_data_);
   }
 
-  std::string toString() {
-    if (is_null_)
-      return "NULL";
-    else if (type_id_ == kTypeInt)
-      return std::to_string(value_.integer_);
-    else if (type_id_ == kTypeFloat)
-      return std::to_string(value_.float_);
-    else {
-      char temp[len_ + 1];
-      memcpy(temp, value_.chars_, len_);
-      temp[len_] = '\0';
-      return {temp};
-    }
-  }
-
- protected:
+protected:
   union Val {
     int32_t integer_;
     float float_;
@@ -156,4 +173,5 @@ class Field {
   bool manage_data_{false};
 };
 
-#endif  // MINISQL_FIELD_H
+
+#endif //MINISQL_FIELD_H
