873ce0eaf467fcef33b92585726c89708c1e6ab9
Weilin Chang <95030066+xishanqiming@users.noreply.github.com>
2023-05-29 23:54:05 +0800

    This is a WRONG CODE, only for experiment

--- src/storage/table_iterator.cpp
+++ src/storage/table_iterator.cpp
@@ -3,47 +3,51 @@
 #include "common/macros.h"
 #include "storage/table_heap.h"
 
-/**
- * TODO: Student Implement
- */
-TableIterator::TableIterator() {
+TableIterator::TableIterator(TableHeap *table_heap, RowId rid, Transaction *txn)
+    : table_heap(table_heap), row(table_heap->GetRow(rid, txn)), txn(txn) {}
 
-}
+TableIterator::TableIterator(const TableIterator &other)
+    : table_heap(other.table_heap), row(other.row), txn(other.txn) {}
 
-TableIterator::TableIterator(const TableIterator &other) {
-
-}
-
-TableIterator::~TableIterator() {
-
-}
+TableIterator::~TableIterator() {}
 
 bool TableIterator::operator==(const TableIterator &itr) const {
-  return false;
+  return this->table_heap == itr.table_heap && this->row == itr.row && this->txn == itr.txn;
 }
 
 bool TableIterator::operator!=(const TableIterator &itr) const {
-  return false;
+  return !(*this == itr);
 }
 
 const Row &TableIterator::operator*() {
-  ASSERT(false, "Not implemented yet.");
+  ASSERT(this->row != nullptr, "Row pointer is null.");
+  return *this->row;
 }
 
 Row *TableIterator::operator->() {
-  return nullptr;
+  ASSERT(this->row != nullptr, "Row pointer is null.");
+  return this->row;
 }
 
 TableIterator &TableIterator::operator=(const TableIterator &itr) noexcept {
-  ASSERT(false, "Not implemented yet.");
+  if (this != &itr) {
+    this->table_heap = itr.table_heap;
+    this->row = itr.row;
+    this->txn = itr.txn;
+  }
+  return *this;
 }
 
 // ++iter
 TableIterator &TableIterator::operator++() {
+  ASSERT(this->row != nullptr && this->txn != nullptr, "Row or transaction pointer is null.");
+  this->row = this->table_heap->GetNextRow(this->row, this->txn);
   return *this;
 }
 
 // iter++
 TableIterator TableIterator::operator++(int) {
-  return TableIterator();
+  TableIterator old_itr = *this;
+  ++*this;
+  return old_itr;
 }
