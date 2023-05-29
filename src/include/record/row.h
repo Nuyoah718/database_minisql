76390a8de62a1be041ca649eed8e4f56d3436498
Weilin Chang <95030066+xishanqiming@users.noreply.github.com>
2023-05-29 08:27:16 +0800

    Update row.h

--- src/include/record/row.h
+++ src/include/record/row.h
@@ -1,16 +1,13 @@
 #ifndef MINISQL_ROW_H
 #define MINISQL_ROW_H
 
-#include <bitset>
-#include <map>
-
 #include <memory>
 #include <vector>
+
 #include "common/macros.h"
 #include "common/rowid.h"
 #include "record/field.h"
 #include "record/schema.h"
-#include "utils/mem_heap.h"
 
 /**
  *  Row format:
@@ -21,57 +18,67 @@
  * --------------------------------------------
  * | Field Nums | Null bitmap |
  * -------------------------------------------
- *  
+ *
  *
  */
-class Row {
+class Row
+{
  public:
   /**
    * Row used for insert
    * Field integrity should check by upper level
    */
-  explicit Row(std::vector<Field> &fields) : heap_(new SimpleMemHeap) {
+  Row(std::vector<Field> &fields)
+  {
     // deep copy
-    for (auto &field : fields) {
-      void *buf = heap_->Allocate(sizeof(Field));
-      fields_.push_back(new (buf) Field(field));
-      if (field.IsNull()) null_nums++;
-    }
-
-    fields_nums = fields.size();
+    for (auto &field : fields)
+      fields_.push_back(new Field(field));
   }
+
+  void destroy()
+  {
+    if (!fields_.empty())
+    {
+      for (auto field : fields_)
+        delete field;
+      fields_.clear();
+    }
+  }
+
+  ~Row() { destroy(); };
 
   /**
    * Row used for deserialize
    */
-  Row() = delete;
+  Row() = default;
 
   /**
    * Row used for deserialize and update
    */
-  Row(RowId rid) : rid_(rid), heap_(new SimpleMemHeap) {}
+  Row(RowId rid) : rid_(rid) {}
 
   /**
-   * Row copy function
+   * Row copy function, deep copy
    */
-  Row(const Row &other) : heap_(new SimpleMemHeap) {
-    if (!fields_.empty()) {
-      for (auto &field : fields_) {
-        heap_->Free(field);
-      }
-      fields_.clear();
-    }
+  Row(const Row &other) {
+    destroy();
     rid_ = other.rid_;
     for (auto &field : other.fields_) {
-      void *buf = heap_->Allocate(sizeof(Field));
-      fields_.push_back(new (buf) Field(*field));
-      if (field->IsNull()) null_nums++;
+      fields_.push_back(new Field(*field));
     }
-
-    fields_nums = other.fields_nums;
   }
 
-  virtual ~Row() { delete heap_; }
+  /**
+   * Assign operator, deep copy
+   */
+  Row &operator=(const Row &other)
+  {
+    destroy();
+    rid_ = other.rid_;
+    for (auto &field : other.fields_)
+      fields_.push_back(new Field(*field));
+    return *this;
+  }
 
   /**
    * Note: Make sure that bytes write to buf is equal to GetSerializedSize()
@@ -80,12 +87,14 @@ class Row {
 
   uint32_t DeserializeFrom(char *buf, Schema *schema);
 
-  /*************************************************************************************\
-   * For empty row, return 0                                                           *
-   * For non-empty row with null fields, eg: |null|null|null|, return header size only *
-   * @return                                                                           *
-  \*************************************************************************************/
+  /**
+   * For empty row, return 0
+   * For non-empty row with null fields, eg: |null|null|null|, return header size only
+   * @return
+   */
   uint32_t GetSerializedSize(Schema *schema) const;
+
+  void GetKeyFromRow(const Schema *schema, const Schema *key_schema, Row &key_row);
 
   inline const RowId GetRowId() const { return rid_; }
 
@@ -93,7 +102,8 @@ class Row {
 
   inline std::vector<Field *> &GetFields() { return fields_; }
 
-  inline Field *GetField(uint32_t idx) const {
+  inline Field *GetField(uint32_t idx) const
+  {
     ASSERT(idx < fields_.size(), "Failed to access field");
     return fields_[idx];
   }
@@ -101,16 +111,8 @@ class Row {
   inline size_t GetFieldCount() const { return fields_.size(); }
 
  private:
-  Row &operator=(const Row &other) = delete;
-
- private:
   RowId rid_{};
-  std::vector<Field *> fields_; /** Make sure that all fields are created by mem heap */
-  MemHeap *heap_{nullptr};
-
-  uint32_t fields_nums{0};
-  uint32_t null_nums{0};
-//  std::bitset<PAGE_SIZE>null_bitmap;
+  std::vector<Field *> fields_; /** Make sure that all field ptr are destructed*/
 };
 
-#endif //MINISQL_TUPLE_H
+#endif  // MINISQL_ROW_H
