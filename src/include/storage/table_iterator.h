503032119d06ba05fb10f3880f47e6b254f2c083
Weilin Chang <95030066+xishanqiming@users.noreply.github.com>
2023-05-29 18:23:02 +0800

    Update table_iterator.h

--- src/include/storage/table_iterator.h
+++ src/include/storage/table_iterator.h
@@ -12,7 +12,7 @@ class TableIterator
  public:
   //构造函数和析构函数
   explicit TableIterator(TableHeap *table_heap, RowId rid, Transaction *txn);
-  TableIterator(const TableIterator &other);
+  explicit TableIterator(const TableIterator &other);
   virtual ~TableIterator();
 
   //运算符重载
@@ -21,14 +21,14 @@ class TableIterator
   const Row &operator*();                             //解引用运算符
   Row *operator->();                                  //成员访问运算符符
   TableIterator &operator++();                        //前自增运算符
-  const TableIterator operator++(int);                //后自增运算符
+  TableIterator operator++(int);                //后自增运算符
   TableIterator &operator=(const TableIterator &itr) noexcept;
 
  private:
   //成员变量
   TableHeap *table_heap{};    //指向TableHeap对象的指针
-  Row *current_row_point{};                 //指向表中当前行的指针
-  Transaction *current_trans_point{};         //指向当前事务的指针
+  Row *row{};                 //指向表中当前行的指针
+  Transaction *txn{};         //指向当前事务的指针
 
   //私有默认构造函数，以防止实例化没有必要的参数
   explicit TableIterator() = default;
