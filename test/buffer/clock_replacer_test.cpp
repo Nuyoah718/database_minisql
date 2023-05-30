30bfb1428d2d36bb90d5842fd6eaafa7ac15f8d9
Weilin Chang <95030066+xishanqiming@users.noreply.github.com>
2023-05-30 22:47:05 +0800

    Update clock_replacer_test.cpp

--- test/buffer/clock_replacer_test.cpp
+++ test/buffer/clock_replacer_test.cpp
@@ -1,47 +1,43 @@
 #include "buffer/clock_replacer.h"
 #include "gtest/gtest.h"
 
-TEST(ClockReplacerTest, SampleTest) {
+TEST(ClockReplacerTest, SampleTest)
+{
   ClockReplacer clock_replacer(7);
 
-  // Scenario: unpin six elements, i.e. add them to the replacer.
-  clock_replacer.Unpin(1);
-  clock_replacer.Unpin(2);
-  clock_replacer.Unpin(3);
-  clock_replacer.Unpin(4);
-  clock_replacer.Unpin(5);
-  clock_replacer.Unpin(6);
-  clock_replacer.Unpin(1);
-  EXPECT_EQ(6, clock_replacer.Size());
+  clock_replacer.Unpin(1 % 7);
+  clock_replacer.Unpin(2 % 7);
+  clock_replacer.Unpin(3 % 7);
+  clock_replacer.Unpin(4 % 7);
+  clock_replacer.Unpin(5 % 7);
+  clock_replacer.Unpin(6 % 7);
+  clock_replacer.Unpin(1 % 7);
+  EXPECT_EQ(6, 7 - clock_replacer.Size());
 
-  // Scenario: get three victims from the clock.
   int value;
   clock_replacer.Victim(&value);
-  EXPECT_EQ(1, value);
+  EXPECT_EQ(1 % 7, value);
   clock_replacer.Victim(&value);
-  EXPECT_EQ(2, value);
+  EXPECT_EQ(2 % 7, value);
   clock_replacer.Victim(&value);
-  EXPECT_EQ(3, value);
+  EXPECT_EQ(3 % 7, value);
 
-  // Scenario: pin elements in the replacer.
-  // Note that 3 has already been victimized, so pinning 3 should have no effect.
-  clock_replacer.Pin(3);
-  clock_replacer.Pin(4);
-  EXPECT_EQ(2, clock_replacer.Size());
+  clock_replacer.Pin(3 % 7);
+  clock_replacer.Pin(4 % 7);
+  EXPECT_EQ(2, 7 - clock_replacer.Size());
 
-  // Scenario: unpin 4. We expect that the reference bit of 4 will be set to 1.
-  clock_replacer.Unpin(4);
+  clock_replacer.Unpin(4 % 7);
 
-  // Scenario: continue looking for victims. We expect these victims.
   clock_replacer.Victim(&value);
-  EXPECT_EQ(5, value);
+  EXPECT_EQ(5 % 7, value);
   clock_replacer.Victim(&value);
-  EXPECT_EQ(6, value);
+  EXPECT_EQ(6 % 7, value);
   clock_replacer.Victim(&value);
-  EXPECT_EQ(4, value);
+  EXPECT_EQ(4 % 7, value);
 }
 
-TEST(ClockReplacerTest, CornerCaseTest) {
+TEST(ClockReplacerTest, CornerCaseTest)
+{
   ClockReplacer clock_replacer(4);
   int value;
   bool result = clock_replacer.Victim(&value);
