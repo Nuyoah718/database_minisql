dd77027f4598835512809df83133f8ef989be2c5
Weilin Chang <95030066+xishanqiming@users.noreply.github.com>
2023-06-07 21:04:57 +0800

    Update row.cpp

--- src/record/row.cpp
+++ src/record/row.cpp
@@ -1,80 +1,90 @@
 #include "record/row.h"
 #include <algorithm>
+#include <cassert>
 
-//对于Row类型对象的序列化，对于为null的Field，可以通过位图的方式标记(即 Null Bitmaps)
-//对于Row类型对象的反序列化，在反序列化每一个Field时，反序列化出来的Field的内存都由该Row对象维护
-//序列化和反序列化函数的返回值为uint32_t类型，它表示在序列化和反序列化过程中buf指针向前推进了多少个字节
-#include "record/row.h"
-
+//序列化函数
 uint32_t Row::SerializeTo(char *buf, Schema *schema) const {
-  ASSERT(schema != nullptr, "Invalid schema before serialize.");
+  ASSERT(schema != nullptr, "Invalid schema before serialize.");    //确保schema不为空
   ASSERT(schema->GetColumnCount() == fields_.size(), "Fields size do not match schema's column size.");
+  //确保fields的大小和schema的列数匹配
 
-  uint32_t ofs = 0;
+  uint32_t offset = 0;
 
-  MACH_WRITE_TO(uint32_t, buf, fields_nums);
-  ofs += sizeof(uint32_t);
-  MACH_WRITE_TO(uint32_t, buf + ofs, null_nums);
-  ofs += sizeof(uint32_t);
+  //写入字段数量和null字段数量
+  memcpy(buf, &fields_nums, sizeof(uint32_t));
+  offset += sizeof(uint32_t);
+  memcpy(buf + offset, &null_nums, sizeof(uint32_t));
+  offset += sizeof(uint32_t);
 
+  //标记null字段
   for (uint32_t i = 0; i < fields_.size(); i++) {
     if (fields_[i]->IsNull()) {
-      MACH_WRITE_INT32(buf + ofs, i);
-      ofs += sizeof(uint32_t);
+      memcpy(buf + offset, &i, sizeof(uint32_t));
+      offset += sizeof(uint32_t);
     }
   }
 
-  for (auto &itr : fields_) {
-    if (!itr->IsNull()) ofs += itr->SerializeTo(buf + ofs);
+  //序列化非null字段
+  for (auto &field : fields_) {
+    if (!field->IsNull()) offset += field->SerializeTo(buf + offset);
   }
 
-  return ofs;
+  return offset;
 }
 
+//反序列化函数
 uint32_t Row::DeserializeFrom(char *buf, Schema *schema) {
-  ASSERT(schema != nullptr, "Invalid schema before serialize.");
-  ASSERT(fields_.empty(), "Non empty field in row.");
+  ASSERT(schema != nullptr, "Invalid schema before serialize.");    //确保schema不为空
+  ASSERT(fields_.empty(), "Non empty field in row.");    //确保fields为空
 
-  uint32_t ofs = 0, i = 0;
+  uint32_t offset = 0;
 
-  fields_nums = MACH_READ_UINT32(buf);
-  ofs += sizeof(uint32_t);
-  null_nums = MACH_READ_UINT32(buf + ofs);
-  ofs += sizeof(uint32_t);
+  //读取字段数量和null字段数量
+  memcpy(&fields_nums, buf, sizeof(uint32_t));
+  offset += sizeof(uint32_t);
+  memcpy(&null_nums, buf + offset, sizeof(uint32_t));
+  offset += sizeof(uint32_t);
 
+  //构建null bitmap
   std::vector<uint32_t> null_bitmap(fields_nums, 0);
-  for (i = 0; i < null_nums; i++) {
-    null_bitmap[MACH_READ_UINT32(buf + ofs)] = 1;
-    ofs += sizeof(uint32_t);
+  for (uint32_t i = 0; i < null_nums; i++) {
+    uint32_t null_index;
+    memcpy(&null_index, buf + offset, sizeof(uint32_t));
+    null_bitmap[null_index] = 1;
+    offset += sizeof(uint32_t);
   }
-  uint32_t fields_nums{0};
-  uint32_t null_nums{0};
-  for (i = 0; i < fields_nums; i++) {
-    fields_.push_back(ALLOC_P(heap, Field)(schema->GetColumn(i)->GetType()));
+
+  //反序列化非null字段
+  for (uint32_t i = 0; i < fields_nums; i++) {
+    auto field = ALLOC_P(heap, Field)(schema->GetColumn(i)->GetType());
+    fields_.push_back(field);
     if (!null_bitmap[i]) {
-      ofs += Field::DeserializeFrom(buf + ofs, schema->GetColumn(i)->GetType(), &fields_[i], false);
+      offset += field->DeserializeFrom(buf + offset, schema->GetColumn(i)->GetType(), &fields_[i], false);
     }
   }
-  return ofs;
+
+  return offset;
 }
 
+//获取序列化大小
 uint32_t Row::GetSerializedSize(Schema *schema) const {
-  ASSERT(schema != nullptr, "Invalid schema before serialize.");
+  ASSERT(schema != nullptr, "Invalid schema before serialize.");    //确保schema不为空
   ASSERT(schema->GetColumnCount() == fields_.size(), "Fields size do not match schema's column size.");
-  if (fields_.empty())
-    return 0;
-  else if (fields_nums == null_nums)
-    return sizeof(uint32_t) * (2 + null_nums);
-  else {
-    uint32_t ofs = 0;
-    for (auto &itr : fields_) {
-      if (!itr->IsNull()) ofs += itr->GetSerializedSize();
-    }
+  //确保fields的大小和schema的列数匹配
 
-    return ofs + sizeof(uint32_t) * (2 + null_nums);
+  //如果fields为空，返回0
+  if (fields_.empty()) return 0;
+
+  uint32_t size = sizeof(uint32_t) * (2 + null_nums);
+  //计算非null字段的序列化大小
+  for (auto &field : fields_) {
+    if (!field->IsNull()) size += field->GetSerializedSize();
   }
+
+  return size;
 }
 
+//从行中获取键
 void Row::GetKeyFromRow(const Schema *schema, const Schema *key_schema, Row &key_row) {
   auto columns = key_schema->GetColumns();
   std::vector<Field> fields;
