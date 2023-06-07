9fce65c0eaf7193f78d6fe3cb9118d2e184099e2
Weilin Chang <95030066+xishanqiming@users.noreply.github.com>
2023-06-07 19:26:31 +0800

    Update clock_replacer.cpp

--- src/buffer/clock_replacer.cpp
+++ src/buffer/clock_replacer.cpp
@@ -1,29 +1,42 @@
 #include "buffer/clock_replacer.h"
 
+/**
+ * 实现了模块一的Bonus目标：
+ * 除LRU Replacer外，实现一种新的缓冲区替换算法（如Clock Replacer）
+ * @author Weilin Chang
+ * @date 06.07.2023
+ */
+
+//构造函数，初始化页面数量
 CLOCKReplacer::CLOCKReplacer(size_t num_pages)
     : capacity(num_pages) {}
 
+//析构函数
 CLOCKReplacer::~CLOCKReplacer() {}
 
 //获取牺牲页
 bool CLOCKReplacer::Victim(frame_id_t *frame_id) {
+  //如果列表为空，则返回false
   if (clock_list.empty())
     return false;
 
-  for (auto it = clock_list.begin(); it != clock_list.end(); ++it) {
+  //遍历列表寻找未被使用的帧
+  auto it = clock_list.begin();
+  while (it != clock_list.end()) {
     if (!clock_status[*it]) {
       *frame_id = *it;
-      clock_list.erase(it);
+      it = clock_list.erase(it);
       clock_status.erase(*frame_id);
       return true;
     }
     else {
-      //重置当前状态为false（表示未被使用）
+      //如果该帧被使用，则重置状态为false（表示未被使用）
       clock_status[*it] = false;
+      ++it;
     }
   }
 
-  //所有的帧都被访问，则选取列表中的第一个帧作为牺牲者
+  //如果所有帧都被使用，则选择列表中的第一个帧作为牺牲者
   *frame_id = clock_list.front();
   clock_list.pop_front();
   clock_status.erase(*frame_id);
@@ -33,6 +46,7 @@ bool CLOCKReplacer::Victim(frame_id_t *frame_id) {
 
 //将帧置为锁定状态
 void CLOCKReplacer::Pin(frame_id_t frame_id) {
+  //如果该帧存在，则移除该帧并清除其状态
   auto it = clock_status.find(frame_id);
   if (it != clock_status.end()) {
     clock_list.remove(frame_id);
@@ -41,17 +55,17 @@ void CLOCKReplacer::Pin(frame_id_t frame_id) {
 }
 
 //取消对帧的锁定
-void CLOCKReplacer::Unpin(frame_id_t frame_id)
-{
+void CLOCKReplacer::Unpin(frame_id_t frame_id) {
+  //如果该帧不存在并且列表未满，则将该帧添加到列表，并设置状态为未使用
   auto it = clock_status.find(frame_id);
   if (it == clock_status.end() && clock_list.size() < capacity) {
     clock_list.push_back(frame_id);
-    clock_status[frame_id] = false; // Set as UNUSED
+    clock_status[frame_id] = false;    //设置为未使用
   }
 }
 
 //计算那些状态不等于EMPTY的数量
-size_t CLOCKReplacer::Size()
-{
+size_t CLOCKReplacer::Size() {
+  //返回非空状态的帧数量
   return clock_status.size();
 }
