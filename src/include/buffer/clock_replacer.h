c4dd379a922022675f8180736d24cb36ff37759d
Weilin Chang <95030066+xishanqiming@users.noreply.github.com>
2023-06-08 18:24:05 +0800

    Update clock_replacer.h

--- src/include/buffer/clock_replacer.h
+++ src/include/buffer/clock_replacer.h
@@ -39,11 +39,16 @@ class CLOCKReplacer : public Replacer {
   size_t Size() override;
 
  private:
+  enum class State { EMPTY, ACCESSED, UNUSED };
+  static bool IsEmpty(CLOCKReplacer::State &);
+
   size_t capacity;
   list<frame_id_t> clock_list;               // replacer中可以被替换的数据页
   map<frame_id_t, frame_id_t> clock_status;  // 数据页的存储状态
   //使用map<frame_id_t, bool>表示clock_status，
   //其中key是frame_id，value表示该帧的访问状态，如果访问过则为true，否则为false
+  std::vector<State> second_chance;
+  frame_id_t pointer{0};
 };
 
 #endif  // MINISQL_CLOCK_REPLACER_H
