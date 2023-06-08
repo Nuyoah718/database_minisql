fcac4e5b40ff43900adf2a3e94d19896179cb62d
Weilin Chang <95030066+xishanqiming@users.noreply.github.com>
2023-06-08 18:28:03 +0800

    Update table_iterator.cpp

--- src/storage/table_iterator.cpp
+++ src/storage/table_iterator.cpp
@@ -1,35 +1,124 @@
-#ifndef MINISQL_TABLE_ITERATOR_H
-#define MINISQL_TABLE_ITERATOR_H
+#include "storage/table_iterator.h"
 
-#include "common/rowid.h"
-#include "record/row.h"
-#include "transaction/transaction.h"
+#include "common/macros.h"
+#include "storage/table_heap.h"
 
-class TableHeap;
 
-class TableIterator {
- private:
-  explicit TableIterator() = default;
+/**
+ * TODO: Student Implement
+ */
+//使用初始化列表来初始化成员
+//如果传入的rid有效，则通过调用table_heap的GetTuple获取元组
+//如果传入的rid无效，则不进行操作
+TableIterator::TableIterator(TableHeap *table_heap, RowId rid, Transaction* txn)
+    : table_heap(table_heap), row(new Row(rid)), txn(txn) {
+  if (rid.GetPageId() != INVALID_PAGE_ID)
+    this->table_heap->GetTuple(row, txn);
+}
 
- public:
-  //构造函数和析构函数
-  explicit TableIterator(TableHeap *table_heap, RowId rid, Transaction *txn);
-  TableIterator(const TableIterator &other);
-  virtual ~TableIterator();
+//拷贝构造函数，创建一个新的TableIterator，并复制other的所有状态
+TableIterator::TableIterator(const TableIterator &other)
+    : table_heap(other.table_heap), row(other.row ? new Row(*other.row) : nullptr), txn(other.txn) {}
 
-  //运算符重载
-  bool operator==(const TableIterator &itr) const;    //检查两个迭代器是否相等
-  bool operator!=(const TableIterator &itr) const;    //检查两个迭代器是否不相等
-  const Row &operator*();                             //解引用运算符
-  Row *operator->();                                  //成员访问运算符符
-  TableIterator &operator++();                        //前自增运算符
-  TableIterator operator++(int);                //后自增运算符
-  TableIterator &operator=(const TableIterator &itr) noexcept;
+//析构函数，释放动态分配的Row
+TableIterator::~TableIterator() {
+  if (row != nullptr) {
+    delete row;
+    row = nullptr;
+  }
+}
 
- private:    //成员变量
-  TableHeap *table_heap{};    //指向TableHeap对象的指针
-  Row *row{};                 //指向表中当前行的指针
-  Transaction *txn{};         //指向当前事务的指针
-};
+TableIterator &TableIterator::operator=(const TableIterator &other) noexcept {
+  if (this != &other) {
+    table_heap = other.table_heap;
+    txn = other.txn;
+    if (row != nullptr) {
+      delete row;
+    }
+    row = other.row ? new Row(*other.row) : nullptr;
+  }
+  return *this;
+}
 
-#endif    //MINISQL_TABLE_ITERATOR_H
+//使用GetRowId的返回值，判断两个TableIterator是否相等
+bool TableIterator::operator==(const TableIterator &itr) const {
+  return this->row->GetRowId().Get() == itr.row->GetRowId().Get();
+}
+
+//使用operator==，判断两个TableIterator是否不等
+bool TableIterator::operator!=(const TableIterator &itr) const {
+  return !this->operator==(itr);
+}
+
+//重载*操作符，返回当前Row的引用
+//在解引用前，先检查当前迭代器是否处于有效状态
+const Row &TableIterator::operator*() {
+  ASSERT(this->operator!=(table_heap->End()), "TableHeap iterator out of range, invalid dereference.");
+  return *this->row;
+}
+
+//重载->操作符，返回当前Row的指针
+//在返回指针前，先检查当前迭代器是否处于有效状态
+Row *TableIterator::operator->() {
+  ASSERT(this->operator!=(table_heap->End()), "TableHeap iterator out of range, invalid dereference.");
+  return this->row;
+}
+
+//重载++操作符
+TableIterator &TableIterator::operator++() {
+  if (table_heap == nullptr || row == nullptr) {
+    throw std::runtime_error("Invalid TableIterator state.");
+  }
+
+  BufferPoolManager *buffer_pool_manager = table_heap->buffer_pool_manager_;
+  auto cur_page = static_cast<TablePage *>(buffer_pool_manager->FetchPage(row->GetRowId().GetPageId()));
+
+  if (cur_page == nullptr) {
+    throw std::runtime_error("Failed to fetch page from buffer pool manager");
+  }
+
+  cur_page->RLatch();
+
+  RowId next_tuple_rid;
+  while (true) {
+    if (cur_page->GetNextTupleRid(row->GetRowId(), &next_tuple_rid) ||
+        (cur_page->GetNextPageId() != INVALID_PAGE_ID &&
+         (cur_page = static_cast<TablePage *>(buffer_pool_manager->FetchPage(cur_page->GetNextPageId()))) != nullptr &&
+         cur_page->GetFirstTupleRid(&next_tuple_rid))) {
+      break;
+    }
+
+    if (cur_page->GetNextPageId() == INVALID_PAGE_ID) {
+      cur_page->RUnlatch();
+      buffer_pool_manager->UnpinPage(cur_page->GetTablePageId(), false);
+      throw std::runtime_error("Failed to find next tuple");
+    }
+
+    cur_page->RUnlatch();
+    buffer_pool_manager->UnpinPage(cur_page->GetTablePageId(), false);
+    cur_page = static_cast<TablePage *>(buffer_pool_manager->FetchPage(cur_page->GetNextPageId()));
+    cur_page->RLatch();
+  }
+
+  if (row != nullptr) {
+    delete row;
+  }
+
+  row = new Row(next_tuple_rid);
+
+  if (*this != table_heap->End()) {
+    table_heap->GetTuple(row, nullptr);
+  }
+
+  cur_page->RUnlatch();
+  buffer_pool_manager->UnpinPage(cur_page->GetTablePageId(), false);
+
+  return *this;
+}
+
+//重载++(int)操作符
+TableIterator TableIterator::operator++(int) {
+  TableIterator temp(*this);    //先保存当前迭代器状态，再执行自增，最后返回保存的旧状态
+  ++(*this);
+  return temp;
+}
