5628c93190175b092b4d031ca7d56ab5dc545169
Weilin Chang <95030066+xishanqiming@users.noreply.github.com>
2023-06-08 18:25:56 +0800

    Update clock_replacer_test.cpp

--- test/buffer/clock_replacer_test.cpp
+++ test/buffer/clock_replacer_test.cpp
@@ -1,44 +1,42 @@
 #include "buffer/clock_replacer.h"
 #include "gtest/gtest.h"
 
-TEST(ClockReplacerTest, SampleTest)
-{
-  ClockReplacer clock_replacer(7);
+TEST(ClockReplacerTest, SampleTest) {
+  CLOCKReplacer clock_replacer(7);
 
-  clock_replacer.Unpin(1 % 7);
-  clock_replacer.Unpin(2 % 7);
-  clock_replacer.Unpin(3 % 7);
-  clock_replacer.Unpin(4 % 7);
-  clock_replacer.Unpin(5 % 7);
-  clock_replacer.Unpin(6 % 7);
-  clock_replacer.Unpin(1 % 7);
-  EXPECT_EQ(6, 7 - clock_replacer.Size());
+  clock_replacer.Unpin(1);
+  clock_replacer.Unpin(2);
+  clock_replacer.Unpin(3);
+  clock_replacer.Unpin(4);
+  clock_replacer.Unpin(5);
+  clock_replacer.Unpin(6);
+  clock_replacer.Unpin(1);
+  EXPECT_EQ(6, clock_replacer.Size());
 
   int value;
   clock_replacer.Victim(&value);
-  EXPECT_EQ(1 % 7, value);
+  EXPECT_EQ(1, value);
   clock_replacer.Victim(&value);
-  EXPECT_EQ(2 % 7, value);
+  EXPECT_EQ(2, value);
   clock_replacer.Victim(&value);
-  EXPECT_EQ(3 % 7, value);
+  EXPECT_EQ(3, value);
 
-  clock_replacer.Pin(3 % 7);
-  clock_replacer.Pin(4 % 7);
-  EXPECT_EQ(2, 7 - clock_replacer.Size());
+  clock_replacer.Pin(3);
+  clock_replacer.Pin(4);
+  EXPECT_EQ(2, clock_replacer.Size());
 
-  clock_replacer.Unpin(4 % 7);
+  clock_replacer.Unpin(4);
 
   clock_replacer.Victim(&value);
-  EXPECT_EQ(5 % 7, value);
+  EXPECT_EQ(5, value);
   clock_replacer.Victim(&value);
-  EXPECT_EQ(6 % 7, value);
+  EXPECT_EQ(6, value);
   clock_replacer.Victim(&value);
-  EXPECT_EQ(4 % 7, value);
+  EXPECT_EQ(4, value);
 }
 
-TEST(ClockReplacerTest, CornerCaseTest)
-{
-  ClockReplacer clock_replacer(4);
+TEST(ClockReplacerTest, CornerCaseTest) {
+  CLOCKReplacer clock_replacer(4);
   int value;
   bool result = clock_replacer.Victim(&value);
   EXPECT_FALSE(result);
