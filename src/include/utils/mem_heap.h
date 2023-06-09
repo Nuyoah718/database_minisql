e0d90273feb84254912c2b917ce99ef3120eabf8
Weilin Chang <xishanqiming@gmail.com>
2023-06-09 19:24:59 +0800

    Update mem_heap.h

--- src/include/utils/mem_heap.h
+++ src/include/utils/mem_heap.h
@@ -9,52 +9,49 @@
 #include <unordered_set>
 #include "common/macros.h"
 
+// MemHeap 类定义了内存分配器的基本接口，
+// SimpleMemHeap 是 MemHeap 的一个简单实现，
+// 它使用 std::unordered_set 进行分配内存的记录。
+
 class MemHeap {
  public:
   virtual ~MemHeap() = default;
 
-  /**
-   * @brief Allocate a contiguous block of memory of the given size
-   * @param size The size (in bytes) of memory to allocate
-   * @return A non-null pointer if allocation is successful. A null pointer if
-   * allocation fails.
-   */
+  // 分配指定大小的内存，返回一个指向分配内存的指针。
   virtual void *Allocate(size_t size) = 0;
 
-  /**
-   * @brief Returns the provided chunk of memory back into the pool
-   */
+  // 释放指定的内存。
   virtual void Free(void *ptr) = 0;
-
 };
 
 class SimpleMemHeap : public MemHeap {
  public:
   ~SimpleMemHeap() {
-    for (auto it: allocated_) {
-      free(it);
+    for (auto allocated_ptr: allocated_memory_set_) {
+      Free(allocated_ptr);
     }
   }
 
-  void *Allocate(size_t size) {
-    void *buf = malloc(size);
-    ASSERT(buf != nullptr, "Out of memory exception");
-    allocated_.insert(buf);
-    return buf;
+  void *Allocate(size_t size) override {
+    void *buffer = malloc(size);
+    ASSERT(buffer != nullptr, "Out of memory exception");
+    allocated_memory_set_.insert(buffer);
+    return buffer;
   }
 
-  void Free(void *ptr) {
+  void Free(void *ptr) override {
     if (ptr == nullptr) {
       return;
     }
-    auto iter = allocated_.find(ptr);
-    if (iter != allocated_.end()) {
-      allocated_.erase(iter);
+    auto iter = allocated_memory_set_.find(ptr);
+    if (iter != allocated_memory_set_.end()) {
+      free(*iter);
+      allocated_memory_set_.erase(iter);
     }
   }
 
  private:
-  std::unordered_set<void *> allocated_;
+  std::unordered_set<void *> allocated_memory_set_;
 };
 
 #endif //MINISQL_MEM_HEAP_H
