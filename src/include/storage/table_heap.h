a38c05d07bfc25ec46ccc21e4ce95b38f9a9fc69
Weilin Chang <xishanqiming@gmail.com>
2023-06-09 21:58:12 +0800

    Update table_heap.h

--- src/include/storage/table_heap.h
+++ src/include/storage/table_heap.h
@@ -113,7 +113,10 @@ private:
           schema_(schema),
           log_manager_(log_manager),
           lock_manager_(lock_manager) {
-    ASSERT(false, "Not implemented yet.");
+    auto first_page = (TablePage *)(buffer_pool_manager_->NewPage(first_page_id_));
+    ASSERT(first_page != nullptr, "Can not initialize the first page.");
+    first_page->Init(first_page_id_, INVALID_PAGE_ID, log_manager_, txn);
+    buffer_pool_manager_->UnpinPage(first_page_id_, true);
   };
 
   explicit TableHeap(BufferPoolManager *buffer_pool_manager, page_id_t first_page_id, Schema *schema,
