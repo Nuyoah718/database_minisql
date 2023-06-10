2150661901302ed1a1463661fcdaef5c3b5c4096
Chengjian Tao <tau@DESKTOP-RN905T5>
2023-06-10 16:20:21 +0800

    Add IndexIterator operator++ and operator*

--- src/index/index_iterator.cpp
+++ src/index/index_iterator.cpp
@@ -17,11 +17,30 @@ IndexIterator::~IndexIterator() {
 }
 
 std::pair<GenericKey *, RowId> IndexIterator::operator*() {
-  ASSERT(false, "Not implemented yet.");
+  return page->GetItem(item_index);
 }
 
 IndexIterator &IndexIterator::operator++() {
-  ASSERT(false, "Not implemented yet.");
+  if (item_index < page->GetSize() - 1) {
+    item_index++;
+  } else if (page->GetNextPageId() != INVALID_PAGE_ID) {
+    page_id_t old_page_id = current_page_id;
+    current_page_id = page->GetNextPageId();
+    page = reinterpret_cast<LeafPage *>(buffer_pool_manager->FetchPage(current_page_id)->GetData());
+
+    /* first on next index */
+    item_index = 0;
+    buffer_pool_manager->UnpinPage(old_page_id, false);
+  } else if (page->GetNextPageId() == INVALID_PAGE_ID) {
+    /* change to index end() */
+    buffer_pool_manager->UnpinPage(current_page_id, false);
+    current_page_id = INVALID_PAGE_ID;
+    page = nullptr;
+    item_index = -1;
+  } else {
+    ASSERT(false, "Can't reach here.");
+  }
+  return *this;
 }
 
 bool IndexIterator::operator==(const IndexIterator &itr) const {
